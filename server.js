367241c349ef5ecd57a2894de91e248ac8cb883a
animatewithk <168434285+animatewithk@users.noreply.github.com>
2024-05-03 18:24:26 +0000

    Updated backend

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs')
+const app = express();
+const port = 1000;
+
+//management of files
+
+//management of requests and responses
+app.get('/', (req, res)=>{
+	res.send('hello world')
+});
+
+app.get('/about', (req, res)=>{
+	res.send('This is about page')
+});
+
+//port configuration
+app.listen(port, ()=>{
+	console.log("the server is started successfully")
+});
