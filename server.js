cefffb0d9dcc6164a4a63b1ba4c029febedcf139
animatewithk <168434285+animatewithk@users.noreply.github.com>
2024-05-04 18:33:47 +0000

    Static website done

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path = require('path')
 
 const app = express()
 
+const StaticPath = path.join(__dirname, 'public')
+app.use(express.static(StaticPath))
 
 app.get('/', (req, res)=>{
-	res.send('success')
+	res.sendFile(path.join(StaticPath, 'index.html'))
 })
-
+app.get('/about', (req, res)=>{
+	res.sendFile(path.join(StaticPath, 'about.html'))
+})
 
 app.listen(1000, ()=>{
 	console.log('done')
